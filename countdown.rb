5b446887c2a023f89a7b22cac4cd05e99b31d2f0
William Famor <jaredfamor@gmail.com>
2019-10-26 02:27:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
-number = 10
-def countdown_to_sleep
-  while number > 0
+number = 10 
+def countdown_with_sleep(number)
+  while seconds < 10
     puts "#{number} SECOND(S)!"
-    number -= 1
-  return "HAPPY NEW YEAR"
-  end
+    number += 1
+    return "HAPPY NEW YEAR!"
+  end 
 end
